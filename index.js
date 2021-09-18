d0a452de8375dfc704037f46b303cc37bc0a241b
Akash <akashshukla506@gmail.com>
2021-09-18 19:33:54 +0530

    Added Edit task Functionality

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const generateTaskCard = ({id, url, title, type, description}) => {
         <div class="card">
             <div class="card-header">
                 <div class="d-flex justify-content-end">
-                    <button type="button" class="btn btn-outline-info">
-                        <i class="fas fa-pencil-alt"></i>
+                    <button type="button" class="btn btn-outline-info" name=${id} onclick="editTask(this)">
+                        <i class="fas fa-pencil-alt" name=${id} onclick="editTask(this)"></i>
                     </button>
                     <button type="button" class="btn btn-outline-danger" name=${id} onclick="deleteTask(this)">
                         <i class="far fa-trash-alt" name=${id} onclick="deleteTask(this)"></i>
@@ -43,14 +43,14 @@ const generateTaskCard = ({id, url, title, type, description}) => {
 }
 
 const saveToLocalStorage = () => {
-    localStorage.setItem("akashtasks", JSON.stringify({akash: globalTaskData}));
+    localStorage.setItem("nikhiltasks", JSON.stringify({nikhil: globalTaskData}));
 }
 
 const reloadTaskCard = () => {
-    const localStorageCopy = JSON.parse(localStorage.getItem("akashtasks"));
+    const localStorageCopy = JSON.parse(localStorage.getItem("nikhiltasks"));
     console.log(localStorageCopy);
     if(localStorageCopy) {
-        globalTaskData = localStorageCopy["akash"];
+        globalTaskData = localStorageCopy["nikhil"];
     }
     console.log(globalTaskData)
     globalTaskData.map((cardData) => {
@@ -64,3 +64,41 @@ const deleteTask = (e) => {
     saveToLocalStorage();
     window.location.reload();
 }
+
+const editTask = (e) => {
+    console.log(e)
+    console.log(e.parentNode)
+    console.log(e.parentNode.parentNode.parentNode.childNodes)
+    console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1])
+    console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[3])
+    console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[5])
+
+    e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1].setAttribute("contenteditable", "true")
+    e.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].setAttribute("contenteditable", "true")
+    e.parentNode.parentNode.parentNode.childNodes[5].childNodes[5].setAttribute("contenteditable", "true")
+
+    console.log(e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1])
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].style.setProperty("border", "1px solid red")
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].style.setProperty("background", "blue")
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("onclick", "saveEditTask(this)")
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML = "SAVE CHANGES"
+}
+
+const saveEditTask = (e) => {
+    const targetID = e.getAttribute("name");
+    console.log(e)
+    // console.log(e.parentNode)
+    // console.log(e.parentNode.parentNode.parentNode.childNodes)
+    // console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1])
+    // console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[3])
+    // console.log(e.parentNode.parentNode.parentNode.childNodes[5].childNodes[5])
+
+    // you will get all the updated fields
+    // then update it inside the localStorage corresponding to that card id
+    // after that saveToLocalStorage
+    // window.location.reload();
+}
+
+
+// var suppose = {id: "1631720624226", url: "egtrshs", title: "tshdj", type: "dyjdyj", description: "dyjydj"}
+// suppose.description = "my name is nikhil agarwal"
